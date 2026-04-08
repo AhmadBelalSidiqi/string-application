4959441cc1e36f49ba23cf633183a3e32a7c1075
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-08 15:17:06 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        //Favorite color
+        String favoriteColor;
+        favoriteColor= "Green";
+        System.out.println("My favorite Color "+favoriteColor);
+
+        // Year-Up Start Year
+        short year;
+        year = 2026;
+        System.out.println("I started Year up in " + year );
+
+        //Middle Initial
+        char middleInitial;
+        middleInitial ='B';
+        System.out.println("My middle initial is: " + middleInitial);
+
+        //Has Pet
+        String hasPet;
+        hasPet = "yes";
+        System.out.println("Do I have a pet "+ hasPet );
+
+        //Nice Message
+        String niceMessage;
+        niceMessage = "Your doing a great job, keep up the good work";
+        System.out.println(niceMessage);
+
+
+
+
+    }
+}
